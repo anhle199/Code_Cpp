41dee2a8494bfc324962f27b311c125714288db9
LeHoangAnh <lehoanganh.le2001@gmail.com>
2020-07-27 23:57:16 +0700

    1

--- VS_C++/Test/Test/main.cpp
+++ VS_C++/Test/Test/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include "BinaryTree.h"
 
 using namespace std;
 
 int main() {
+	BinaryTree<int> b;
+
+	int x;
+
+	for (int i = 0; i < 5; i++) {
+		cout << "Enter: ";
+		cin >> x;
+		
+		b.add(x);
+	}
+
+	cout << BinaryTree<int>::size << endl;
 
 	return 0;
 }

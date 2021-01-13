9657684eb93f5a1dad99cd7fdd50827154de7e13
Lê Hoàng Anh <lehoanganh.le2001@gmail.com>
2021-01-13 17:56:34 +0700

    Update SieveEratosthenes.cpp

--- Library/Algorithms/SieveEratosthenes.cpp
+++ Library/Algorithms/SieveEratosthenes.cpp
@@ -1,33 +1,13 @@
-// In các số nguyên tố từ 2 đến n bằng phương pháp sàng Eratosthenes
-#include <iostream>
-#include <vector>
-#include <cmath>
+vector<bool> sieveEratosthenes(int n) {
+	vector<bool> primes(n + 1, true);
+	primes[0] = primes[1] = false;
 
-using namespace std;
+	for (int i = 2; i * i <= n; i++) {
+		if (primes[i])
+			if ((long long)i * (long long)i <= n) // check overflow.
+				for (int j = i * i; j <= n; j += i)
+					primes[j] = false;
+	}
 
-void sieveEratosthenes(vector<bool> &checkPrimes, int &n)
-{
-    for (int i = 2; i <= sqrt(n); i++)
-        if (checkPrimes[i])
-            for (int j = i * i; j < n + 1; j += i)
-                checkPrimes[j] = false;
-}
-
-int main()
-{
-    int n;
-
-    cout << "Nhap n = ";
-    cin >> n;
-
-    vector<bool> checkPrimes(n + 1, true);
-    sieveEratosthenes(checkPrimes, n);
-    
-    cout << "Cac so nguyen to tu 2 den n la: ";
-    for (int i = 2; i < n + 1; i++)
-        if (checkPrimes[i])
-            cout << i << " ";
-    cout << endl;
-
-    return 0;
+	return primes;
 }
